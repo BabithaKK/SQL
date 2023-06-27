5c83c7cf205f3d9a92256ffb835129330b1f3529
babitha <babithakk.xworkz@gmail.com>
2023-06-27 14:31:49 +0530

    hey

---
+++ Lake.java
@@ -0,0 +1,48 @@
+public class Lake{
+    public static void main(String[] args) {
+        String lakeName = "Lake Tahoe";
+        int lakeDepth = 501;
+        boolean isNatural = true;
+        double lakeArea = 191.6;
+        float lakeVolume = 150.7f;
+        char lakeType = 'F';
+        byte lakeFishCount = 12;
+        short lakeElevation = 6225;
+        long lakeID = 9876543210L;
+        String lakeLocation = "California, Nevada";
+        String lakeTypeOfWater = "Freshwater";
+        String lakeColor = "Blue";
+        String lakeShape = "Irregular";
+        String lakeInlets = "63";
+        String lakeOutlet = "Truckee River";
+        String lakeUses = "Recreation, Water Supply";
+        String lakeWildlife = "Trout, Salmon, Bears";
+        String lakeClimate = "Temperate";
+        String lakeSurroundings = "Forests, Mountains";
+        String lakeRecreation = "Boating, Fishing, Hiking";
+        String lakeWaterQuality = "Good";
+        boolean isProtected = true;
+        System.out.println("Lake Name: " + lakeName);
+        System.out.println("Lake Depth: " + lakeDepth);
+        System.out.println("Is Natural: " + isNatural);
+        System.out.println("Lake Area: " + lakeArea);
+        System.out.println("Lake Volume: " + lakeVolume);
+        System.out.println("Lake Type: " + lakeType);
+        System.out.println("Lake Fish Count: " + lakeFishCount);
+        System.out.println("Lake Elevation: " + lakeElevation);
+        System.out.println("Lake ID: " + lakeID);
+        System.out.println("Lake Location: " + lakeLocation);
+        System.out.println("Lake Type of Water: " + lakeTypeOfWater);
+        System.out.println("Lake Color: " + lakeColor);
+        System.out.println("Lake Shape: " + lakeShape);
+        System.out.println("Lake Inlets: " + lakeInlets);
+        System.out.println("Lake Outlet: " + lakeOutlet);
+        System.out.println("Lake Uses: " + lakeUses);
+        System.out.println("Lake Wildlife: " + lakeWildlife);
+        System.out.println("Lake Climate: " + lakeClimate);
+        System.out.println("Lake Surroundings: " + lakeSurroundings);
+        System.out.println("Lake Recreation: " + lakeRecreation);
+        System.out.println("Lake Water Quality: " + lakeWaterQuality);
+        System.out.println("Is Protected: " + isProtected);
+    }
+}
